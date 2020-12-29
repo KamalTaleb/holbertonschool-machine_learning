216642cf1b66277ad2c939038921968b936f4754
Kamal Taleb <kamal.talebb@gmail.com>
2020-12-29 16:14:00 +0200

    neuron

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -7,12 +7,12 @@ import numpy as np
 class Neuron:
     """the number of input features to the neuron"""
 
-    def __init__(self, n):
+    def __init__(self, nx):
         """constructor"""
-        if not isinstance(n, int):
-            raise TypeError("the number should be an integer")
-        if n < 1:
-            raise ValueError("the number should be a positive integer")
-        self.W = np.random.normal(0, 1, (1, n))
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.normal(0, 1, (1, nx))
         self.b = 0
         self.A = 0
