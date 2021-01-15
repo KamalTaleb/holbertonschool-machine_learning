9d207626263dbe0add45a21514cb60dec24b94ad
Kamal Taleb <kamal.talebb@gmail.com>
2021-01-15 15:38:26 +0200

    Privatize NeuralNetwork

--- supervised_learning/0x01-classification/9-neural_network.py
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -5,7 +5,11 @@ import numpy as np
 
 
 class NeuralNetwork:
-    """NeuralNetwork class"""
+    """
+    NeuralNetwork class
+    defines a neural network with one hidden layer
+    performing binary classification
+    """
 
     def __init__(self, nx, nodes):
         """
@@ -21,50 +25,56 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        """The weights vector for the hidden layer"""
-        self.W1 = np.random.normal(0, 1, (nodes, nx))
+        """The weights vector for the hidden layer. Upon instantiation,
+            it should be initialized using a random normal distribution"""
+        self.__W1 = np.random.normal(0, 1, (nodes, nx))
 
-        """The bias for the hidden layer"""
-        self.b1 = np.zeros((nodes, 1))
+        """The bias for the hidden layer. Upon instantiation,
+            it should be initialized with 0’s"""
+        self.__b1 = np.zeros((nodes, 1))
 
-        """The activated output for the hidden layer"""
-        self.A1 = 0
+        """The activated output for the hidden layer. Upon instantiation,
+            it should be initialized to 0"""
+        self.__A1 = 0
 
-        """The weights vector for the output neuron"""
-        self.W2 = np.random.normal(0, 1, (1, nodes))
+        """The weights vector for the output neuron. Upon instantiation,
+            it should be initialized using a random normal distribution"""
+        self.__W2 = np.random.normal(0, 1, (1, nodes))
 
-        """The bias for the output neuron"""
-        self.b2 = 0
+        """The bias for the output neuron. Upon instantiation,
+            it should be initialized to 0"""
+        self.__b2 = 0
 
-        """The activated output for the output neuron (prediction)"""
-        self.A2 = 0
+        """The activated output for the output neuron (prediction).
+            Upon instantiation, it should be initialized to 0"""
+        self.__A2 = 0
 
     @property
     def W1(self):
-        """weights to retrieve W1"""
+        """property to retrieve W1"""
         return self.__W1
 
     @property
     def b1(self):
-        """bias to retrieve b1"""
+        """property to retrieve b1"""
         return self.__b1
 
     @property
     def A1(self):
-        """activation to retrieve A1"""
+        """property to retrieve A1"""
         return self.__A1
 
     @property
     def W2(self):
-        """weights to retrieve W2"""
+        """property to retrieve W2"""
         return self.__W2
 
     @property
     def b2(self):
-        """bias to retrieve b2"""
+        """property to retrieve b2"""
         return self.__b2
 
     @property
     def A2(self):
-        """activation to retrieve A2"""
+        """property to retrieve A2"""
         return self.__A2
