44cf54868e793d6a729e020efb84e871b20b573b
Kamal Taleb <kamal.talebb@gmail.com>
2021-01-15 15:33:03 +0200

    Neural network

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Contains the NeuralNetwork"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """NeuralNetwork class"""
+
+    def __init__(self, nx, nodes):
+        """
+        constructor
+        :param nx: number of input features
+        :param nodes: number of nodes found in the hidden layer
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        """The weights vector for the hidden layer"""
+        self.W1 = np.random.normal(0, 1, (nodes, nx))
+
+        """The bias for the hidden layer"""
+        self.b1 = np.zeros((nodes, 1))
+
+        """The activated output for the hidden layer"""
+        self.A1 = 0
+
+        """The weights vector for the output neuron"""
+        self.W2 = np.random.normal(0, 1, (1, nodes))
+
+        """The bias for the output neuron"""
+        self.b2 = 0
+
+        """The activated output for the output neuron (prediction)"""
+        self.A2 = 0
