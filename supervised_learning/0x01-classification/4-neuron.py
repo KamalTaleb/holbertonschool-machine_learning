07ee757017cbefbf4a7a8c9257dd72f2200e9a47
Kamal Taleb <kamal.talebb@gmail.com>
2021-01-15 14:42:44 +0200

    evaluate neuron

--- supervised_learning/0x01-classification/4-neuron.py
+++ supervised_learning/0x01-classification/4-neuron.py
@@ -11,9 +11,7 @@ def sigmoid(Z):
 
 
 class Neuron:
-    """Neuron class
-    nx is the number of input features to the neuron
-    """
+    """the number of input features to the neuron"""
 
     def __init__(self, nx):
         """constructor"""
@@ -22,61 +20,41 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        """The weights vector for the neuron. Upon instantiation,
-        it should be initialized using a random normal distribution."""
         self.__W = np.random.normal(0, 1, (1, nx))
-
-        """The bias for the neuron. Upon instantiation,
-        it should be initialized to 0."""
         self.__b = 0
-
-        """The activated output of the neuron (prediction). Upon instantiation,
-        it should be initialized to 0."""
         self.__A = 0
 
     @property
     def W(self):
-        """property to retrieve it"""
+        """weights to retrieve it"""
         return self.__W
 
     @property
     def b(self):
-        """property to retrieve it"""
+        """bias to retrieve it"""
         return self.__b
 
     @property
     def A(self):
-        """property to retrieve it"""
+        """activation to retrieve it"""
         return self.__A
 
     def forward_prop(self, X):
         """Calculates the forward propagation of the neuron
-        X is a numpy.ndarray with shape (nx, m) that contains the input data
-        """
+        X"""
         Z = np.matmul(self.__W, X) + self.__b
         """self.__A = 1 / (1 + np.exp(-Z))"""
         self.__A = sigmoid(Z)
         return self.__A
 
     def cost(self, Y, A):
-        """Calculates the cost of the model using logistic regression
-        Y is a numpy.ndarray with shape (1, m)
-        that contains the correct labels for the input data
-        A is a numpy.ndarray with shape (1, m)
-        containing the activated output of the neuron for each example
-        """
+        """Calculates the cost of the model using logistic regression"""
         cost = -np.sum((Y * np.log(A)) +
                        ((1 - Y) * np.log(1.0000001 - A))) / Y.shape[1]
         return cost
 
     def evaluate(self, X, Y):
-        """Evaluates the neuron’s predictions
-        X is a numpy.ndarray with shape (nx, m) that contains the input data
-        nx is the number of input features to the neuron
-        m is the number of examples
-        Y is a numpy.ndarray with shape (1, m)
-        that contains the correct labels for the input data
-        """
+        """Evaluates the neuron’s predictions X"""
         self.forward_prop(X)
         A = np.where(self.__A >= 0.5, 1, 0)
         cost = self.cost(Y, self.__A)
