c4bd0c76f42dc85998cc3ef3584711ea5eb8d2a5
Kamal Taleb <kamal.talebb@gmail.com>
2021-01-15 14:37:04 +0200

    evaluate neuron

--- supervised_learning/0x01-classification/4-neuron.py
+++ supervised_learning/0x01-classification/4-neuron.py
@@ -1,50 +1,83 @@
 #!/usr/bin/env python3
-"""Evaluates the neuron’s prediction"""
+"""Contains the Neuron class"""
+
 import numpy as np
 
 
-class Neuron:
-    """Neuron class"""
-    def __init__(self, nx):
-        """Defines a single neuron performing binary classification"""
-        if not isinstance(nx, int):
-            raise TypeError("nx must be a integer")
-        if nx < 1:
-            raise ValueError("nx must be positive")
+# Miscellaneous functions
+def sigmoid(Z):
+    """sigmoid function"""
+    return 1.0 / (1.0 + np.exp(-Z))
 
-        self.__W = np.random.normal(size=(nx, 1))
+
+class Neuron:
+    """Neuron class
+    nx is the number of input features to the neuron
+    """
+
+    def __init__(self, nx):
+        """constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        """The weights vector for the neuron. Upon instantiation,
+        it should be initialized using a random normal distribution."""
+        self.__W = np.random.normal(0, 1, (1, nx))
+
+        """The bias for the neuron. Upon instantiation,
+        it should be initialized to 0."""
         self.__b = 0
+
+        """The activated output of the neuron (prediction). Upon instantiation,
+        it should be initialized to 0."""
         self.__A = 0
 
     @property
     def W(self):
-        """Weights"""
+        """property to retrieve it"""
         return self.__W
 
     @property
     def b(self):
-        """Bias"""
+        """property to retrieve it"""
         return self.__b
 
     @property
     def A(self):
-        """Activation"""
+        """property to retrieve it"""
         return self.__A
 
     def forward_prop(self, X):
-        """Calculates the forward propagation of the neuron"""
-        z = np.dot(self.__W.T, X)
-        self.__A = 1/(1+np.exp(-z))
+        """Calculates the forward propagation of the neuron
+        X is a numpy.ndarray with shape (nx, m) that contains the input data
+        """
+        Z = np.matmul(self.__W, X) + self.__b
+        """self.__A = 1 / (1 + np.exp(-Z))"""
+        self.__A = sigmoid(Z)
         return self.__A
 
     def cost(self, Y, A):
-        """Calculates the cost of the model using logistic regression"""
-        cost = (-Y * np.log(A) - (1 - Y) * np.log(1 - A)).mean()
+        """Calculates the cost of the model using logistic regression
+        Y is a numpy.ndarray with shape (1, m)
+        that contains the correct labels for the input data
+        A is a numpy.ndarray with shape (1, m)
+        containing the activated output of the neuron for each example
+        """
+        cost = -np.sum((Y * np.log(A)) +
+                       ((1 - Y) * np.log(1.0000001 - A))) / Y.shape[1]
         return cost
 
     def evaluate(self, X, Y):
-        """Evaluates the neuron’s predictions"""
-        A = self.forward_prop(X)
-        cost = self.cost(Y, A)
-        pred_labels = np.where(A >= 0.5, 1, 0)
-        return pred_labels, cost
+        """Evaluates the neuron’s predictions
+        X is a numpy.ndarray with shape (nx, m) that contains the input data
+        nx is the number of input features to the neuron
+        m is the number of examples
+        Y is a numpy.ndarray with shape (1, m)
+        that contains the correct labels for the input data
+        """
+        self.forward_prop(X)
+        A = np.where(self.__A >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__A)
+        return A, cost
