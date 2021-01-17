b50e66e8f99094031181c1d1f30e15fc82d971a8
Kamal Taleb <kamal.talebb@gmail.com>
2021-01-17 18:16:59 +0200

    momentum

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
-"""Contains the update_variables_momentum function"""
+"""Contains the create_momentum_op function"""
+
+import tensorflow as tf
 
 
-def update_variables_momentum(alpha, beta1, var, grad, v):
+def create_momentum_op(loss, alpha, beta1):
     """
-    updates a variable using the gradient descent with
-    momentum optimization algorithm
+    creates the training operation for a neural network in tensorflow using
+    the gradient descent with momentum optimization algorithm:
+    :param loss: loss of the network
     :param alpha: learning rate
     :param beta1: momentum weight
-    :param var: numpy.ndarray containing the variable to be updated
-    :param grad: numpy.ndarray containing the gradient of var
-    :param v: previous first moment of var
-    :return: updated variable and the new moment, respectively
+    :return: the momentum optimization operation
     """
-    # Exponentially Weighted Averages
-    v = beta1 * v + (1 - beta1) * grad
-
-    # variable update
-    var = var - alpha * v
-    return var, v
+    optimizer = tf.train.MomentumOptimizer(alpha, beta1)
+    return optimizer.minimize(loss)
