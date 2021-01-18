b2c313c069445e4d632da81259d6406127cc283a
Kamal Taleb <kamal.talebb@gmail.com>
2021-01-18 20:20:51 +0200

    create confusion

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -5,8 +5,7 @@ import numpy as np
 
 
 def create_confusion_matrix(labels, logits):
-    """
-    creates a confusion matrix
+    """creates a confusion matrix
     :param labels: one-hot numpy.ndarray of shape (m, classes)
         containing the correct labels for each data point
             m is the number of data points
