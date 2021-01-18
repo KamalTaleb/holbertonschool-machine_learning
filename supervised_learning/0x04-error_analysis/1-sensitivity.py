5966408de16be4ca9ad2ca6fcec424d584376518
Kamal Taleb <kamal.talebb@gmail.com>
2021-01-18 20:21:52 +0200

    sensitivity

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -5,8 +5,7 @@ import numpy as np
 
 
 def sensitivity(confusion):
-    """
-    calculates the sensitivity for each class in a confusion matrix:
+    """calculates the sensitivity for each class in a confusion matrix:
     :param confusion: confusion numpy.ndarray of shape (classes, classes)
     where row indices represent the correct labels and column indices
     represent the predicted labels
