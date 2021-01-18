514e5a225abcbd83bc580d88d3b7a31c537af12d
Kamal Taleb <kamal.talebb@gmail.com>
2021-01-18 20:22:33 +0200

    precision

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -5,8 +5,7 @@ import numpy as np
 
 
 def precision(confusion):
-    """
-    :param confusion: is a confusion numpy.ndarray of shape (classes, classes)
+    """:param confusion: is a confusion numpy.ndarray of shape (classes, classes)
         where row indices represent the correct
         labels and column indices represent the predicted labels
     :return: numpy.ndarray of shape (classes,)
