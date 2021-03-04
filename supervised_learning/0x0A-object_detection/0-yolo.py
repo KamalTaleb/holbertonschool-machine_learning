eafd0a157460603c45017ad4610272d4386c0c75
Kamal Taleb <kamal.talebb@gmail.com>
2021-03-04 23:22:25 +0200

    initialize yolo

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
-"""the Yolo class"""
+"""contains the Yolo class"""
 
 import tensorflow.keras as K
 import numpy as np
 
 
 class Yolo:
-    """the Yolo v3 algorithm"""
+    """
+    uses the Yolo v3 algorithm to perform object detection
+    """
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """
         class constructor
         :param self:
-        :param model_path: is the path to where a Darknet Keras model is stored
-        :param classes_path: is the path to where the list of class names used for the Darknet model, listed in order of index, can be found
-        :param class_t: is a float representing the box score threshold for the initial filtering step
-        :param nms_t: is a float representing the IOU threshold
+        :param model_path: path to where a Darknet Keras model is stored
+        :param classes_path: path to where the list of class names used
+            for the Darknet model, listed in order of index, can be found
+        :param class_t: float representing the box score threshold
+            for the initial filtering step
+        :param nms_t: float representing the IOU threshold
             for non-max suppression
-        :param anchors: is a numpy.ndarray of shape (outputs, anchor_boxes, 2)
+        :param anchors: numpy.ndarray of shape (outputs, anchor_boxes, 2)
             containing all of the anchor boxes:
             outputs is the number of outputs (predictions)
                 made by the Darknet model
